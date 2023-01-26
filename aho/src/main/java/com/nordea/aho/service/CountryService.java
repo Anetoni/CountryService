5e5b8f0caada066159a715940e14064a49315890
Anetoni <aho.anttoni@gmail.com>
2023-01-26 16:25:18 +0200

    service class added

---
+++ aho/src/main/java/com/nordea/aho/service/CountryService.java
@@ -0,0 +1,31 @@
+package com.nordea.aho.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.nordea.aho.model.Country;
+
+@Service
+public class CountryService {
+    
+    private ArrayList<Country> countries;
+
+    public CountryService() {
+        countries = new ArrayList<Country>();
+
+    
+    }
+
+    public Country getCountry(String name) {
+        for (Country country : countries) {
+            if(name.equals(country.getCountry())) {
+                return country;
+            }
+        }
+        return null;
+    }
+}
