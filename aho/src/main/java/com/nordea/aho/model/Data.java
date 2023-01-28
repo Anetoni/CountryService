2d40b7d383552d94a23c5b080e275e8aab5901d8
Anetoni <aho.anttoni@gmail.com>
2023-01-28 23:11:30 +0200

    added fields

--- aho/src/main/java/com/nordea/aho/model/Data.java
+++ aho/src/main/java/com/nordea/aho/model/Data.java
@@ -3,6 +3,7 @@ package com.nordea.aho.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 /*
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
+@JsonPropertyOrder({"name", "iso2", "currency"})
 public class Data {
     @JsonProperty("name")
     private String name;
@@ -18,10 +20,13 @@ public class Data {
     private String iso2;
     @JsonProperty("capital")
     private String capital;
-    @JsonProperty("population")
+    @JsonProperty("populationCounts")
     private String population;
     @JsonProperty("flag")
     private String flag_file_url;
+    private String currency;
+    private String dialCode;
+    private String unicodeFlag;
     
     public String getName() {
         return name;
@@ -61,5 +66,29 @@ public class Data {
 
     public void setFlag_file_url(String flag_file_url) {
         this.flag_file_url = flag_file_url;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getDialCode() {
+        return dialCode;
+    }
+
+    public void setDialCode(String dialCode) {
+        this.dialCode = dialCode;
+    }
+
+    public String getUnicodeFlag() {
+        return unicodeFlag;
+    }
+
+    public void setUnicodeFlag(String unicodeFlag) {
+        this.unicodeFlag = unicodeFlag;
     }
 }
