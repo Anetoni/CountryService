900459cdf36f2c3f6d3f25054697459c783109e3
Anetoni <aho.anttoni@gmail.com>
2023-01-28 12:20:02 +0200

    simplified country class

--- aho/src/main/java/com/nordea/aho/model/Country.java
+++ aho/src/main/java/com/nordea/aho/model/Country.java
@@ -1,70 +1,21 @@
 package com.nordea.aho.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 /*
  * This class is used to handle information fetched from external sources
  */
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Country {
-    
-    private String country;    
-    private String code;
-    private String capital;
-    private String population;
-    private String flag_file_url;
+    private List<Data> countries = null;
 
-    public Country() {
-        
+    public List<Data> getData() {
+        return countries;
     }
 
-    public Country(String country, String code, String capital, String population, String flag_file_url) {
-        this.country = country;
-        this.code = code;
-        this.capital = capital;
-        this.population = population;
-        this.flag_file_url = flag_file_url;
-    }
-
-    public String getFlag_file_url() {
-        return flag_file_url;
-    }
-
-    public void setFlag_file_url(String flag_file_url) {
-        this.flag_file_url = flag_file_url;
-    }
-
-   
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getCapital() {
-        return capital;
-    }
-
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
-    public String getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(String population) {
-        this.population = population;
+    public void setData(List<Data> countries) {
+        this.countries = countries;
     }
 }
