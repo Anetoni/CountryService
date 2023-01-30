826c72c7d7e04822222aa27393334fcd4efca345
Anetoni <aho.anttoni@gmail.com>
2023-01-30 12:42:54 +0200

    tests written

--- aho/src/test/java/com/nordea/aho/CountryServiceApplicationTests.java
+++ aho/src/test/java/com/nordea/aho/CountryServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package com.nordea.aho;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import com.nordea.aho.model.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nordea.aho.model.Countries;
+import com.nordea.aho.service.CountryService;
 
 @SpringBootTest
 class CountryServiceApplicationTests {
 
+	//Should retrieve a list of all countries starting with Afghanistan
 	@Test
-	void contextLoads() {
+	void shouldRetrieveCountries() throws Exception {
+		CountryService countryService = new CountryService();
+		Countries countries = countryService.getCountries();
+		List<Data> countryList = countries.getData();
+		assertEquals("Afghanistan", countryList.get(0).getName());
 	}
 
+	//Should retrieve Finland's information
+	@Test
+	void shouldRetrieveFinland() throws Exception {
+		CountryService countryService = new CountryService();
+		Data finland = countryService.getCountry("Finland");
+		assertEquals("Finland", finland.getName());
+		assertEquals("EUR", finland.getCurrency());
+	}
 }
